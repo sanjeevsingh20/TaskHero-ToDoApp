17cfbddec811a935d200ad273a503b35b6608e36
Sanjeev singh <sk2747267@gmail.com>
2023-08-06 13:50:37 +0530

    Update confidentila.js

--- backend/config/confidentila.js
+++ backend/config/confidentila.js
@@ -1,7 +1,7 @@
-const emai_password = "ryjkmvqagolvmrkd";
-const email_user = "manojkumarsinghy20@gmail.com";
-const secret_key = "yametekhudasai";
-const data_password = "@#Sanjukumar123";
+const emai_password = "Your mail Password provided to developers by gmail ";
+const email_user = "YOur@gmail.com";
+const secret_key = "YOur_secret_key";
+const data_password = "database Password(if any)";
 
 module.exports = {
   emai_password,
