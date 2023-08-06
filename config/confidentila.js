3ae274f2cd394277e20c1a96095f5bc072bac627
Sanjeev singh <sk2747267@gmail.com>
2023-08-06 13:46:03 +0530

    Add files via upload

---
+++ config/confidentila.js
@@ -0,0 +1,11 @@
+const emai_password = "ryjkmvqagolvmrkd";
+const email_user = "manojkumarsinghy20@gmail.com";
+const secret_key = "yametekhudasai";
+const data_password = "@#Sanjukumar123";
+
+module.exports = {
+  emai_password,
+  email_user,
+  secret_key,
+  data_password,
+};
